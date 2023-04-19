c0a3cec8135be0a17e5e6461608c861761294dc3
will <allwood.w2003@gmail.com>
2023-04-19 19:03:16 +0100

    errorr handling for main()

--- 2023Assignment/Source.cpp
+++ 2023Assignment/Source.cpp
@@ -2,11 +2,24 @@
 
 using namespace std;
 
+
 int main()
 {
     string playerName;
-    cout << "Please enter your name: ";
-    cin >> playerName;
+    bool validName = false;
+
+    while (!validName) {
+        cout << "Please enter your name (max 20 characters): ";
+        cin >> playerName;
+
+        // Check if the player's name is longer than 20 characters
+        if (playerName.length() > 20) {
+            cout << "\nSorry your name cannot be longer than 20 characters please try again.\n" << endl;
+        }
+        else {
+            validName = true;
+        }
+    }
 
     userInterface UI;
     UI.startQuiz();
